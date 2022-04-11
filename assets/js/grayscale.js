31a8f5dca7c68497777b35a54134036014f3c03e
millonariorx <betterbeat00@gmail.com>
2022-04-11 14:54:54 -0500

    feat: add files and project

---
+++ assets/js/grayscale.js
@@ -0,0 +1,65 @@
+(function() {
+  "use strict"; // Start of use strict
+
+  var mainNav = document.querySelector('#mainNav');
+
+  if (mainNav) {
+
+    var navbarCollapse = mainNav.querySelector('.navbar-collapse');
+    
+    if (navbarCollapse) {
+
+      var collapse = new bootstrap.Collapse(navbarCollapse, {
+        toggle: false
+      });
+      
+      var navbarItems = navbarCollapse.querySelectorAll('a');
+      
+      // Closes responsive menu when a scroll trigger link is clicked
+      for (var item of navbarItems) {
+        item.addEventListener('click', function (event) {
+          collapse.hide();
+        });
+      }
+    }
+
+    // Collapse Navbar
+    var collapseNavbar = function() {
+
+      var scrollTop = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
+
+      if (scrollTop > 100) {
+        mainNav.classList.add("navbar-shrink");
+      } else {
+        mainNav.classList.remove("navbar-shrink");
+      }
+    };
+    // Collapse now if page is not at top
+    collapseNavbar();
+    // Collapse the navbar when page is scrolled
+    document.addEventListener("scroll", collapseNavbar);
+  }
+
+})(); // End of use strict
+
+
+document.addEventListener('DOMContentLoaded', () => {
+
+  // Unix timestamp (in seconds) to count down to
+  var twoDaysFromNow = (new Date().getTime() / 1000) + (86400 * 2) + 1;
+
+  // Set up FlipDown
+  var flipdown = new FlipDown(twoDaysFromNow)
+
+    // Start the countdown
+    .start()
+
+    // Do something when the countdown ends
+    .ifEnded(() => {
+      console.log('The countdown has ended!');
+    });
+
+  
+  var ver = document.getElementById('ver');
+  ver.innerHTML = flipdown.version;
+});
