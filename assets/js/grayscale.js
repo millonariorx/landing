91ef72274da76cfeb547940f2a775d1f8fbd53fb
cristian <cristian2182062@correo.uis.edu.co>
2022-04-17 14:12:20 -0500

    feat(logo_details): add logo image in navbar and add banner opacity background

--- assets/js/grayscale.js
+++ assets/js/grayscale.js
@@ -53,7 +53,7 @@ var texto="Hola ya he realizado el pago";
 document.addEventListener('DOMContentLoaded', () => {
 
   // Unix timestamp (in seconds) to count down to
-  var twoDaysFromNow = (new Date().getTime() / 1000) + (86400 * 2) + 1;
+  var twoDaysFromNow = (new Date().getTime() / 1000) + (1800) + 1;
 
   // Set up FlipDown
   var flipdown = new FlipDown(twoDaysFromNow)
